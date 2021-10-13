8352352ccf964daff8667fa2cd8b66cc69b2eb32
AlpacajinB <89957317+AlpacajinB@users.noreply.github.com>
2021-10-13 17:09:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken =InVxSRbxFjIbM25D6SOOOX1chTe2bchnQrzIK+3OpCt6jit5g+7EASattuSYZC5DBq1T16Cc0QxWaxEtSAan/X3p8Z9iAQ0bU
+lineaccesstoken = 'IKnVxSRbxFjIbM25D6SOOOX1chTe2bchnQrzIK+3OpCt6jit5g+7EASattuSYZC5DBq1T16Cc0QxWaxEtSAan/X3p8Z9iAQ0bU+MvrAfCuhBgR6tXpeGWtfny'
+
 line_bot_api = LineBotApi(lineaccesstoken)
 
 # APP CONFIGURATIONS
